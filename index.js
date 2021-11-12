fc9775a81ece8adc2a708fb187170366ef097a71
hoangnam1402 <18521130@gm.uit.edu.vn>
2021-11-13 01:49:11 +0700

    react

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
 var express = require('express');
 var app = express();
+
+app.use(express.static("public"));
+app.set("view engine", "ejs");
+app.set("views", "./interface");
+
+app.listen(3000);
+
+app.get("/", (req, res) => {
+    res.render("test");
+})

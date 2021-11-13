fba93e4ee01eda36e9085b6600292515b8f71b47
hoangnam1402 <18521130@gm.uit.edu.vn>
2021-11-13 10:21:19 +0700

    init back-end

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
-var express = require('express');
-var app = express();
+const express = require('express');
+const app = express();
+const PORT = 3000;
 
 app.use(express.static("public"));
 app.set("view engine", "ejs");
 app.set("views", "./interface");
 
-app.listen(3000);
-
 app.get("/", (req, res) => {
     res.render("test");
 })
+
+app.listen(PORT, ()=>{
+    console.log(`Listening at http://localhost:${port}`)
+});

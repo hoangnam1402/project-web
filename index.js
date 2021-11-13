d6b6d448090ea0c0ea09c6ee9157fb38482aa8b2
hoangnam1402 <18521130@gm.uit.edu.vn>
2021-11-13 10:37:28 +0700

    connect db

--- index.js
+++ index.js
@@ -1,6 +1,25 @@
 const express = require('express');
+const moongoose = require('moongoose');
+
+require('dotenv').config()
 const app = express();
 const PORT = 3000;
+
+const MONGO_URI=`mongodb+srv://${process.env.dbUser}:${process.env.dbPw}@cluster0.mbklg.mongodb.net/${process.env.dbName}`;
+const connectBD = async () => {
+    try {
+        await mongoose.connect(MONGO_URI, {
+            useCreateIndex: true,
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+            useFindAndModify: false,
+        });
+        console.log('Connect success');
+    } catch (error){
+        console.log(error,message)
+        process.exit(1);
+    }
+}
 
 app.use(express.static("public"));
 app.set("view engine", "ejs");
